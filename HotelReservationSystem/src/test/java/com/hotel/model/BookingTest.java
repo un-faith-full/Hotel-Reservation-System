7db2904b0853ea89d651053fddb64ea7ecda6c14
un-faith-full <shoaib.60373@iqra.edu.pk>
2026-01-21 16:48:45 +0500

    Update BookingTest.java

--- HotelReservationSystem/src/test/java/com/hotel/model/BookingTest.java
+++ HotelReservationSystem/src/test/java/com/hotel/model/BookingTest.java
@@ -48,7 +48,7 @@ class BookingTest {
     
     @Test
     void calculateTotalPrice_ValidBooking_ShouldCalculateCorrectly() {
-        Customer customer = new Customer("CUST001", "John Doe", "john@email.com");
+        Customer customer = new Customer("CUST001", "John Doe", "jon@email.com");
         Room room = new Room("101", RoomType.SINGLE, 100.0);
         LocalDate checkIn = LocalDate.now().plusDays(1);
         LocalDate checkOut = LocalDate.now().plusDays(4);
